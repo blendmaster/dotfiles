122e2742d40c00768d32cb884657311492f6d427
Steven Ruppert <blendmaster@gmail.com>
2012-12-15 23:12:51 -0700

    Apply smartBorders to xmonad, fullscreen
    Still doesn't let firefox go fullscreen. Probably something with the
    unity panel/shell, lame. Not sure how to reconcile having the useful
    gnome/unity applets in the top right, while maintaining xmonad
    usability, which seems pretty nice.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,15 +1,21 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Util.EZConfig
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
 
 myManageHook = composeAll (
   [ manageHook gnomeConfig
   , className =? "Unity-2d-panel" --> doIgnore
   , className =? "Unity-2d-shell" --> doFloat
+  , isFullscreen --> doFullFloat
   ])
+
+myLayoutHook = smartBorders $ layoutHook gnomeConfig
 
 main = xmonad $ gnomeConfig
   { manageHook = myManageHook
+  , layoutHook = myLayoutHook
   , modMask = mod4Mask
   }
   `additionalKeys`
